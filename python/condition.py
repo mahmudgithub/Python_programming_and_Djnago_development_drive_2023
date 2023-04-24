7779552ac995644e2d8403045949dc9be6cfabbd
mahmudgithub <mahmudhossain838@gmail.com>
2023-04-24 15:13:35 +0100

    short hand if else

--- python/condition.py
+++ python/condition.py
@@ -26,11 +26,24 @@
 # fun(one,two)
 
 
+# def fun(a,b):
+#     if a!=b:
+#         print("a is not equal b")
+#     else:
+#         print("equal")
+# one=int(input("enter first number:"))
+# two=int(input("enter second number:"))
+# fun(one,two)
+
+# Short Hand If
+# If you have only one statement to execute, 
+# you can put it on the same line as the if statement.
+# a=5
+# b=3
+# if a > b:print("hello mim")
+
 def fun(a,b):
-    if a!=b:
-        print("a is not equal b")
-    else:
-        print("equal")
+    print ("i love you mim") if a>b else print("jaf love mim")
 one=int(input("enter first number:"))
 two=int(input("enter second number:"))
 fun(one,two)
